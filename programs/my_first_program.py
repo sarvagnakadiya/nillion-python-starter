e0002e3a21748dee075d009f263dfb7f8ffb18ec
sarvagnakadiya <sarvagnakadiya1@gmail.com>
2024-07-02 05:47:29 +0000

    arathmatic operation with nada added

--- programs/my_first_program.py
+++ programs/my_first_program.py
@@ -6,7 +6,17 @@ def nada_main():
     my_int1 = SecretInteger(Input(name="my_int1", party=party1))
     my_int2 = SecretInteger(Input(name="my_int2", party=party1))
 
-    # write the computation for your program here - use my_int1 and my_int2 as inputs
-    # make sure you change the output below to be your new output
+    # Complex arithmetic operations
+    addition = my_int1 + my_int2
+    subtraction = my_int1 - my_int2
+    multiplication = my_int1 * my_int2
+    division = my_int1 / my_int2  # Note: Ensure my_int2 is not zero to avoid division by zero error
+    modulus = my_int1 % my_int2
 
-    return [Output(my_int1, "my_output", party1)]
+    return [
+        Output(addition, "addition_output", party1),
+        Output(subtraction, "subtraction_output", party1),
+        Output(multiplication, "multiplication_output", party1),
+        Output(division, "division_output", party1),
+        Output(modulus, "modulus_output", party1)
+    ]
